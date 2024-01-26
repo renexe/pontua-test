16bd69f541821aeb275f0e2c02904a32e82909cd
Renê Lima <rene.exe@gmail.com>
2024-01-26 10:53:05 -0300

    <fix> Fixing the input text color

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -35,7 +35,7 @@ const Input = forwardRef<HTMLInputElement, InputProps>(
         <input
           type={type === 'password' ? (isPasswordVisible ? 'text' : 'password') : type}
           className={cn(
-            "font-epilogue flex items-center h-[59px] w-full rounded-sm border-[0.70px] text-blue-500 font-bold text-sm tracking-tighter border-gray-400 bg-white px-[17px] py-6 transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-xs placeholder:font-normal placeholder:text-gray-400 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-blue-500 disabled:cursor-not-allowed disabled:opacity-50",
+            "font-epilogue flex items-center h-[59px] w-full rounded-sm border-[0.70px] text-blue-500 font-bold text-sm tracking-tighter border-gray-400 bg-white px-[17px] py-6 file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-xs placeholder:font-normal placeholder:text-gray-400 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-blue-500 disabled:cursor-not-allowed disabled:opacity-50",
             className
           )}
           ref={ref}
