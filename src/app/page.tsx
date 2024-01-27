7e290220b2acd46f4c9c501545bb71346b6bdf97
Renê Lima <rene.exe@gmail.com>
2024-01-26 21:24:31 -0300

    <refac> Compounding the modal component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 import Image from "next/image";
-import Modal from "@/components/ui/organisms/modal";
+import { Modal, ModalContent, ModalDescription, ModalFooter, ModalTitle } from "@/components/ui/organisms/modal";
+import { Typography } from "@/components/ui/atoms/typography";
+import FormLogin from "@/components/ui/molecules/form-login";
+import Link from "next/link";
+import ShieldIcon from "@/components/ui/atoms/icons/shield-icon";
 
 export default function Home() {
 
@@ -24,7 +28,24 @@ export default function Home() {
             className=""
           />
         </div>
-        <Modal />
+        <Modal>
+          <ModalTitle>
+            Bem-vindo<span className="text-orange-500">.</span>
+          </ModalTitle>
+          <ModalDescription>
+            informe as suas credenciais de acesso ao portal
+          </ModalDescription>
+          <ModalContent>
+            <FormLogin />
+          </ModalContent>
+          <ModalFooter>
+            <Link href="/forgot-password" className="hover:underline underline-offset-2">
+              <Typography color="orange" variant="body2" className="w-full flex justify-end gap-1 mt-5">
+                <ShieldIcon /> Esqueceu sua senha?
+              </Typography>
+            </Link>
+          </ModalFooter>
+        </Modal>
       </div>
     </main>
   );
--- src/components/ui/organisms/modal.tsx
+++ src/components/ui/organisms/modal.tsx
@@ -1,27 +1,39 @@
-import Link from "next/link"
 import { Typography } from "../atoms/typography"
-import ShieldIcon from "../atoms/icons/shield-icon"
-import FormLogin from "../molecules/form-login"
 
-const Modal = () => {
+export interface ModalProps {
+  children: React.ReactNode
+}
+
+const Modal = ({ children }: ModalProps) => {
   return (
     <div className="bg-white w-[380px] rounded flex flex-col px-9 py-12">
-      <Typography variant="h1">
-        Bem-vindo<span className="text-orange-500">.</span>
-      </Typography>
-      <Typography color="gray" className="mb-[6px]">
-        informe as suas credenciais de acesso ao portal
-      </Typography>
-
-      <FormLogin />
-
-      <Link href="/forgot-password" className="hover:underline underline-offset-2">
-        <Typography color="orange" variant="body2" className="w-full flex justify-end gap-1 mt-5">
-          <ShieldIcon /> Esqueceu sua senha?
-        </Typography>
-      </Link>
+      {children}
     </div>
   )
 }
 
-export default Modal;
+const ModalTitle = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <Typography variant="h1">
+      {children}
+    </Typography>
+  )
+}
+
+const ModalDescription = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <Typography color="gray" className="mb-[6px]">
+      {children}
+    </Typography>
+  )
+}
+
+const ModalContent = ({ children }: { children: React.ReactNode }) => {
+  return (<>{children}</>)
+}
+
+const ModalFooter = ({ children }: { children: React.ReactNode }) => {
+  return (<>{children}</>)
+}
+
+export { Modal, ModalTitle, ModalDescription, ModalContent, ModalFooter };
