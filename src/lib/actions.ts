2b717919f00bd96c8a3fa59016e86a7682649967
Renê Lima <rene.exe@gmail.com>
2024-01-28 19:06:50 -0300

    <feat> Crafting the signIn action

---
+++ src/lib/actions.ts
@@ -0,0 +1,27 @@
+"use server";
+
+import { redirect } from "next/navigation";
+import { cookies } from "next/headers";
+
+const mockedUser = {
+  name: "Douglas",
+  email: "douglas@pontua.com.br",
+  password: "123456",
+  token:
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c",
+};
+
+export async function signIn(formData: FormData) {
+  const formDataObject = Object.fromEntries(formData.entries());
+  const submitedEmail = formDataObject.email;
+  const submitedPassword = formDataObject.password;
+
+  if (
+    submitedEmail === mockedUser.email &&
+    submitedPassword === mockedUser.password
+  ) {
+    cookies().set("session", mockedUser.token);
+    return true;
+  }
+  return false;
+}
