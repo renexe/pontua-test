8437e9e5e122ec1ea6e95c85f04d9a70822120d2
Renê Lima <rene.exe@gmail.com>
2024-01-28 19:07:28 -0300

    <feat> Crafting the onboarding-template-controller

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import OnboardingScreen from "@/components/ui/pages/onboarding-screen";
 
 export default async function Home() {
+  
   return (
     <main className="min-h-screen bg-blue-800">
       <OnboardingScreen />
