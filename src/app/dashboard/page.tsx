cc801c513a4c8c755895c420abf0083afab30eb1
Renê Lima <rene.exe@gmail.com>
2024-01-30 20:14:35 -0300

    <feat> Add functionaloty to the search input

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,4 @@
-import AmplifyIcon from "@/components/ui/atoms/icons/amplify";
-import { Input } from "@/components/ui/atoms/input";
-import { Typography } from "@/components/ui/atoms/typography";
-import ProfileMenu from "@/components/ui/molecules/profile-menu";
-import { cn } from "@/lib/utils";
-import Image from "next/image";
-import Link from "next/link";
+import DashboardHomeScreen from "@/components/ui/pages/dashboard-home-screen";
 
 async function getHeroes() {
   const res = await fetch('https://gateway.marvel.com/v1/public/characters?ts=1&apikey=84d1bd4d3a025bf80b0d30ad747f85b8&hash=649c4fe1ca33ed38c47a7e08a732fa8a&limit=100')
@@ -20,57 +14,9 @@ export default async function DashboardHome() {
   const data = await getHeroes();
   const heroes = data.data.results;
 
-  // Separate heroes by pages of 10 items per page
-  const pages: any[] = [];
-  for (let i = 0; i < heroes.length; i += 10) {
-    const page = heroes.slice(i, i + 10);
-    pages.push(page);
-  }
-
-  console.log(pages);
-
-  const descriptionFactory = (description: string) => {
-    if (description === "") {
-      return "Sem descrição";
-    }
-
-    if (description.length > 100) {
-      return `${description.slice(0, 100)}...`;
-    }
-
-    return description;
-  }
-
   return (
     <section className="w-full">
-      <div className="flex w-full h-16 items-center overflow-hidden pl-9 border-b border-gray-50">
-        <AmplifyIcon />
-        <Input placeholder="Busque um agente" className="min-w-[70vw] h-full border-none focus-visible:ring-0" />
-      </div>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-2 mt-5 mx-9">
-        {pages[0].map((hero: any, index: number) => (
-          <div key={hero.id} className={cn(index >= pages[0].length - 2 ? "col-span-1 sm:col-span-2" : "col-span-1")}>
-            <Link
-              href={`/dashboard/perfil/${hero.id}`}
-              className="w-full h-[150px] bg-gray-200 rounded-xl flex p-3 gap-4 shadow-md hover:scale-95 transition-transform hover:border hover:border-gray-400"
-            >
-              <div className="relative w-[80px] h-full">
-                <Image
-                  src={`${hero.thumbnail.path}.${hero.thumbnail.extension}`}
-                  alt={hero.name}
-                  sizes="99vw"
-                  fill
-                  className="rounded-2xl object-cover"
-                />
-              </div>
-              <div className="flex flex-col flex-1 h-full overflow-hidden">
-                <Typography variant="h5">{hero.name}</Typography>
-                <Typography className="mt-2">{descriptionFactory(hero.description)}</Typography>
-              </div>
-            </Link>
-          </div>
-        ))}
-      </div>
+      <DashboardHomeScreen heroes={heroes} />
     </section>
   );
 }
---
+++ src/components/ui/pages/dashboard-home-screen.tsx
@@ -0,0 +1,84 @@
+'use client';
+import { useEffect, useState } from "react";
+import { Input } from "@/components/ui/atoms/input";
+import { Typography } from "@/components/ui/atoms/typography";
+import { cn } from "@/lib/utils";
+import AmplifyIcon from "@/components/ui/atoms/icons/amplify";
+import Image from "next/image";
+import Link from "next/link";
+
+
+export interface DashboardHomeScreenProps {
+  heroes: any[];
+}
+
+const DashboardHomeScreen = ({ heroes }: DashboardHomeScreenProps) => {
+  const [search, setSearch] = useState("");
+  const [currentHeroes, setCurrentHeroes] = useState(heroes);
+  const [pages, setPages] = useState<any[][]>([]);
+  const [currentPage, setCurrentPage] = useState(0);
+
+  useEffect(() => {
+    const filteredHeroes = heroes.filter((hero) => hero.name.toLowerCase().includes(search.toLowerCase()));
+    const pages = [];
+    const pageSize = 10;
+    for (let i = 0; i < filteredHeroes.length; i += pageSize) {
+      const page = filteredHeroes.slice(i, i + pageSize);
+      pages.push(page);
+    }
+
+    setPages(pages);
+  }, [search, heroes]);
+
+  const descriptionFactory = (description: string) => {
+    if (description === "") {
+      return "Sem descrição";
+    }
+
+    if (description.length > 100) {
+      return `${description.slice(0, 100)}...`;
+    }
+
+    return description;
+  }
+
+  const handleSearch = (target: HTMLInputElement) => {
+    setSearch(target.value);
+  }
+
+  return (
+    <>
+      <div className="flex w-full h-16 items-center overflow-hidden pl-9 border-b border-gray-50">
+        <AmplifyIcon />
+        <Input placeholder="Busque um agente" value={search} onChange={(e) => handleSearch(e.target)} className="min-w-[70vw] h-full border-none focus-visible:ring-0" />
+      </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-2 mt-5 mx-9">
+        {pages.length > 0 &&
+          pages[currentPage].map((hero: any, index: number) => (
+            <div key={hero.id} className={cn(index >= 8 ? "col-span-1 sm:col-span-2" : "col-span-1")}>
+              <Link
+                href={`/dashboard/perfil/${hero.id}`}
+                className="w-full h-[150px] bg-gray-200 rounded-xl flex p-3 gap-4 shadow-md hover:shadow-lg transition-transform hover:border hover:border-gray-400"
+              >
+                <div className="relative w-[80px] h-full">
+                  <Image
+                    src={`${hero.thumbnail.path}.${hero.thumbnail.extension}`}
+                    alt={hero.name}
+                    sizes="99vw"
+                    fill
+                    className="rounded-2xl object-cover"
+                  />
+                </div>
+                <div className="flex flex-col flex-1 h-full overflow-hidden">
+                  <Typography variant="h5">{hero.name}</Typography>
+                  <Typography className="mt-2">{descriptionFactory(hero.description)}</Typography>
+                </div>
+              </Link>
+            </div>
+          ))}
+      </div>
+    </>
+  );
+}
+
+export default DashboardHomeScreen;
