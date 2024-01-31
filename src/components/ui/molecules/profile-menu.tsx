a3aeeccf7b74efb45799be412ad842fc78aabe58
Renê Lima <rene.exe@gmail.com>
2024-01-31 19:08:57 -0300

    <feat> Changing the profile-menu to be dynamic

--- src/components/ui/molecules/profile-menu.tsx
+++ src/components/ui/molecules/profile-menu.tsx
@@ -7,10 +7,9 @@ import Image from "next/image"
 
 export const HERO_MENU = [
   "Visão Geral",
-  "Teams",
-  "Powers",
-  "Species",
-  "Authors",
+  "Comics",
+  "Series",
+  "Stories",
 ]
 
 export interface ProfileMenuProps {
@@ -23,6 +22,89 @@ const ProfileMenu = ({ hero }: ProfileMenuProps) => {
 
   const handleMenuClick = (menu: string) => {
     setCurrentMenu(menu)
+  }
+
+  console.log(hero)
+
+  const splitChunks = (array: any[], size: number) => {
+    const chunks = [];
+    for (let i = 0; i < array.length; i += size) {
+      chunks.push(array.slice(i, i + size));
+    }
+    return chunks;
+  }
+
+  const Panels = () => {
+    switch (currentMenu) {
+      case "Visão Geral":
+        return (
+          <>
+            <div className="relative h-[90px] w-[90px] md:h-[120px] md:w-[120px] rounded-full overflow-hidden">
+              <Image
+                src={hero.thumbnail.path + '.' + hero.thumbnail.extension}
+                alt=""
+                fill
+                sizes="99vw"
+                className="cover-image"
+              />
+            </div>
+            <div className="flex-1">
+              <Typography variant="h3" className="text-blue-600">{hero.name}</Typography>
+              <Typography className="text-gray-500 text-sm font-semibold mt-4">{hero.description ? hero.description : 'Descrição não fornecida.'}</Typography>
+            </div>
+          </>
+        )
+      case "Comics":
+        const splitedComics = splitChunks(hero.comics.items, 10)
+
+        return (
+          <div className="flex flex-wrap gap-12">
+            {splitedComics.map((chunk, index) => (
+              <ul key={index} className="list-disc max-h-[80%] mr-4">
+                {chunk.map((comic: any) => (
+                  <li key={comic.name}>
+                    <Typography className="text-sm font-semibold text-gray-500">{comic.name}</Typography>
+                  </li>
+                ))}
+              </ul>
+            ))}
+          </div>
+        )
+      case "Series":
+        const splitedSeries = splitChunks(hero.series.items, 10)
+        
+        return (
+          <div className="flex flex-wrap gap-12">
+            {splitedSeries.map((chunk, index) => (
+              <ul key={index} className="list-disc max-h-[80%] mr-4">
+                {chunk.map((serie: any) => (
+                  <li key={serie.name}>
+                    <Typography className="text-sm font-semibold text-gray-500">{serie.name}</Typography>
+                  </li>
+                ))}
+              </ul>
+            ))}
+          </div>
+        )
+      case "Stories":
+        const splitedStories = splitChunks(hero.stories.items, 10)
+
+        return (
+          <div className="flex flex-wrap gap-12">
+            {splitedStories.map((chunk, index) => (
+              <ul key={index} className="list-disc max-h-[80%] mr-4">
+                {chunk.map((story: any) => (
+                  <li key={story.name}>
+                    <Typography className="text-sm font-semibold text-gray-500">{story.name}</Typography>
+                  </li>
+                ))}
+              </ul>
+            ))}
+          </div>
+        )
+      default:
+        return null
+    }
   }
 
   return (
@@ -43,19 +125,7 @@ const ProfileMenu = ({ hero }: ProfileMenuProps) => {
         </ul>
       </nav>
       <div className="mt-7 rounded-2xl py-10 px-8 mr-14 shadow-lg flex gap-7">
-        <div className="relative h-[90px] w-[90px] md:h-[120px] md:w-[120px] rounded-full overflow-hidden">
-          <Image
-            src={hero.thumbnail.path + '.' + hero.thumbnail.extension}
-            alt=""
-            fill
-            sizes="99vw"
-            className="cover-image"
-          />
-        </div>
-        <div>
-          <Typography variant="h3" className="text-blue-600">{hero.name}</Typography>
-          <Typography className="text-gray-500 text-sm font-semibold mt-4">{hero.description ? hero.description : 'Descrição não fornecida.'}</Typography>
-        </div>
+        <Panels />
       </div>
     </>
   )
