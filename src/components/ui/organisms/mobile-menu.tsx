034d6727f0b4114bd0b5299adcb5c4837174f292
Renê Lima <rene.exe@gmail.com>
2024-02-01 09:48:35 -0300

    <fix> Adjusting the mobile dashboard menu

--- src/components/ui/organisms/mobile-menu.tsx
+++ src/components/ui/organisms/mobile-menu.tsx
@@ -2,7 +2,7 @@ import DashboardMenuNav from "../molecules/dashboard-menu-nav"
 
 const MobileMenu = () => {
   return (
-    <div className="bg-gray-100 w-full h-16 fixed top-0 left-0 sm:hidden">
+    <div className="bg-gray-100 w-full h-16 fixed top-0 left-0 sm:hidden z-50 shadow">
       <DashboardMenuNav />
     </div>
   )
