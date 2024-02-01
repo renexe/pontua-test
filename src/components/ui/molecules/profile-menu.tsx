80cfba93d0fa32ee05c9b825df8e548712bb74b7
Renê Lima <rene.exe@gmail.com>
2024-01-31 22:41:35 -0300

    <fix> Adjusting styles of profile nav

--- src/components/ui/molecules/profile-menu.tsx
+++ src/components/ui/molecules/profile-menu.tsx
@@ -17,27 +17,24 @@ export interface ProfileMenuProps {
   handleMenuClick: (menu: string) => void
 }
 
-const ProfileMenu = ({ currentMenu, handleMenuClick }: ProfileMenuProps) => { 
+const ProfileMenu = ({ currentMenu, handleMenuClick }: ProfileMenuProps) => {
 
   return (
-    <>
-      <nav>
-        <ul className="flex gap-6 mt-6 border-b border-b-gray-50">
-          {HERO_MENU.map((item) => (
-            <li
-              key={item}
-              className={cn("pb-4 cursor-pointer", currentMenu === item && "border-b-2 border-blue-600")}
-              onClick={() => handleMenuClick(item)}
-            >
-              <Typography variant="h6" key={item} className={cn("px-1", currentMenu === item ? "text-blue-600" : "text-gray-500")}>
-                {item}
-              </Typography>
-            </li>
-          ))}
-        </ul>
-      </nav>
-     
-    </>
+    <nav>
+      <ul className="flex mt-6 border-b border-b-gray-50">
+        {HERO_MENU.map((item) => (
+          <li
+            key={item}
+            className={cn("pb-4 cursor-pointer group hover:bg-slate-50 px-3 pt-3", currentMenu === item && "border-b-2 border-blue-600")}
+            onClick={() => handleMenuClick(item)}
+          >
+            <Typography variant="h6" key={item} className={cn("px-1 group-hover:text-blue-800", currentMenu === item ? "text-blue-600" : "text-gray-500")}>
+              {item}
+            </Typography>
+          </li>
+        ))}
+      </ul>
+    </nav>
   )
 }
 
--- src/components/ui/organisms/profile-content.tsx
+++ src/components/ui/organisms/profile-content.tsx
@@ -1,10 +1,11 @@
 'use client'
 
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { Typography } from "../atoms/typography"
 import ProfileMenu, { HERO_MENU } from "../molecules/profile-menu";
 import Image from "next/image"
 import { ProfileContentPanel, ProfileContentPanelList } from "../molecules/profile-content-panel";
+import { animated, useSpring } from '@react-spring/web'
 
 export interface ProfileContentProps {
   hero: any
@@ -30,29 +31,46 @@ const ProfileContent = ({ hero }: ProfileContentProps) => {
     }
   }
 
+  const [panelKey, setPanelKey] = useState(0);
+
+  const { opacity } = useSpring({
+    from: { opacity: 0 },
+    to: { opacity: 1 },
+    key: panelKey,
+    reset: true,
+  })
+
+  useEffect(() => {
+    setPanelKey(panelKey + 1)
+  }, [currentMenu])
+
   return (
     <>
       <ProfileMenu currentMenu={currentMenu} handleMenuClick={handleMenuClick} />
-      <ProfileContentPanel>
-        {currentMenu === "Visão Geral" ? (
-          <>
-            <div className="relative h-[90px] w-[90px] md:h-[120px] md:w-[120px] rounded-full overflow-hidden">
-              <Image
-                src={hero.thumbnail.path + '.' + hero.thumbnail.extension}
-                alt=""
-                fill
-                sizes="99vw"
-                priority
-                className="cover-image"
-              />
-            </div>
-            <div className="flex-1">
-              <Typography variant="h3" className="text-blue-600">{hero.name}</Typography>
-              <Typography className="text-gray-500 text-sm font-semibold mt-4">{hero.description ? hero.description : 'Descrição não encontrada.'}</Typography>
-            </div>
-          </>
-        ) : <ProfileContentPanelList data={determinePanelListContent()} />}
-      </ProfileContentPanel>
+      <animated.div
+        style={{ opacity }}
+      >
+        <ProfileContentPanel>
+          {currentMenu === "Visão Geral" ? (
+            <>
+              <div className="relative h-[90px] w-[90px] md:h-[120px] md:w-[120px] rounded-full overflow-hidden">
+                <Image
+                  src={hero.thumbnail.path + '.' + hero.thumbnail.extension}
+                  alt=""
+                  fill
+                  sizes="99vw"
+                  priority
+                  className="object-cover"
+                />
+              </div>
+              <div className="flex-1">
+                <Typography variant="h3" className="text-blue-600">{hero.name}</Typography>
+                <Typography className="text-gray-500 text-sm font-semibold mt-4">{hero.description ? hero.description : 'Descrição não encontrada.'}</Typography>
+              </div>
+            </>
+          ) : <ProfileContentPanelList data={determinePanelListContent()} />}
+        </ProfileContentPanel>
+      </animated.div>
     </>
   );
 }
